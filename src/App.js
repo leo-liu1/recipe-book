326c2973a59fce667aca8b9ff7e5de4047e0acf9
Xuan Peng <44904830+syeaziran@users.noreply.github.com>
2020-11-21 14:24:32 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import firebase from 'firebase';
+import "firebase/auth"
 
 import Navbar from './components/navigation/Navbar';
 
