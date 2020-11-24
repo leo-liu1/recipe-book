e4ab834403e6930afe078f8cbef79eafdd8324a4
leo-liu1 <leoliu1@g.ucla.edu>
2020-11-24 01:40:28 -0800

    pushed in navbar margins, changed icons for navbar

--- src/components/navigation/Navbar.js
+++ src/components/navigation/Navbar.js
@@ -12,30 +12,32 @@ export default function Navbar({ isAuthenticated }) {
     const { logout } = useAuth();
 
     return (<nav className="navigation">
-        <div className="left">
-            <Link to="/" className="title">
-                <FridgeIcon className="logo-icon"/>
-                <div className="text">
-                    Recipe to Cook
-                </div>
-            </Link>
-        </div>
-        <NavbarSearch />
-        <div className="right">
-            {isAuthenticated ?
-                (<>
-                    <Link to="/bookmarks" className="navbar-link">
-                        <BookmarksIcon className="navbar-icon"/>
-                    </Link>
-                    <Link to="/recommendations" className="navbar-link">
-                        <RecommendationsIcon className="navbar-icon"/>
-                    </Link>
-                    <div className="logout" onClick={() => {logout(); console.log("logged out")}}>Logout</div>
-                </>) :
-                (<div className="login">
-                    <Link to="/login">Login</Link>
-                    <Link to="/signup" className="signup">Sign Up</Link>
-                </div>)}
+        <div className="container">
+            <div className="left">
+                <Link to="/" className="title">
+                    <FridgeIcon className="logo-icon"/>
+                    <div className="text">
+                        Recipe to Cook
+                    </div>
+                </Link>
+            </div>
+            <NavbarSearch />
+            <div className="right">
+                {isAuthenticated ?
+                    (<>
+                        <Link to="/bookmarks" className="navbar-link">
+                            <BookmarksIcon className="navbar-icon"/>
+                        </Link>
+                        <Link to="/recommendations" className="navbar-link">
+                            <RecommendationsIcon className="navbar-icon"/>
+                        </Link>
+                        <div className="logout" onClick={() => {logout(); console.log("logged out")}}>Logout</div>
+                    </>) :
+                    (<div className="login">
+                        <Link to="/login">Login</Link>
+                        <Link to="/signup" className="signup">Sign Up</Link>
+                    </div>)}
+            </div>
         </div>
     </nav>);
 }
