7a3e5cb69dfa969fc418ed1c2ad9a65069e7cfa9
Xuan Peng <44904830+syeaziran@users.noreply.github.com>
2020-12-01 15:16:08 -0800

    Update Search.js
    testing get data

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,17 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect} from "react";
 import { useLocation } from 'react-router-dom';
+import SpoonacularHandler from '../components/handlers/SpoonacularHandler.js';
 
 export default function Search() {
     document.title = "Search";
+    const [list, setList] = useState([])
 
     const location = useLocation();
     const urlParams = new URLSearchParams(location.search);
     const query = urlParams.get('q');
+    const ingredientList = query.split(', ');
+    console.log(ingredientList);
+
+    //const ingredientsString = ingredientList.join(",+");
+    //console.log(ingredientsString);
+    async function getData(){
+      const API_Key="1717d4742a794307904ef77c8515e2ad";
+      const spoonacularObj = new SpoonacularHandler(API_Key);
+      setList(await spoonacularObj.searchRecipeByIngredients(ingredientList));
+    }
+    useEffect(()=>{
+      getData();
+    },[]);
+
+    console.log(list);
 
     return (<div className="search">
         <div className="page-title">
             <div className="page-title-text">You Searched for {query}</div>
         </div>
-        <div className="search-container"></div>
+        <div className="search-container">
+
+        </div>
     </div>);
 }
