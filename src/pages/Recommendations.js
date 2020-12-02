bafc906cece4d64cfa7999eadf61b9ed8dd5af7a
JingyiZuo <jzuo1998@gmail.com>
2020-12-02 01:05:00 -0800

    Update Recommendations.js

--- src/pages/Recommendations.js
+++ src/pages/Recommendations.js
@@ -3,19 +3,31 @@ import React, { useRef, useState, useEffect } from 'react';
 import Recipe from '../components/classes/Recipe';
 import { useRecommend } from '../components/handlers/RecommendationsHandler';
 import { useFirestore } from '../components/handlers/FirestoreHandler';
+import { useSpoonacular } from '../components/handlers/SpoonacularHandler';
 import Ingredient from '../components/classes/Ingredient';
 
 export default function Recommendations() {
     document.title = "Recommendations";
     const [posts, setPosts] = useState([]);
+    const [ids, setIDs] = useState([]);
     const { addUserIngredient, removeUserIngredient, getAllUserIngredients, getRecipeHistory } = useFirestore();
+    const { searchSimilarRecipes } = useSpoonacular();
+    
 
     useEffect(() => {
         
-          getRecipeHistory().then((res) => res).then(data => setPosts(data))
+          //getRecipeHistory().then(data => setPosts(data))
+          getRecipeHistory().then(data => {
+              for(var i = 0; i < data.length; i++) {
+                var obj = data[i];
+                
+                console.log(obj.recipeID);
+}
+          })
+          //searchSimilarRecipes("")
           
     }); 
-    
+
     return (<div className="recommendations">
         <div className="page-title">Your Recommendations</div>
         <div className="recommendations-container">
